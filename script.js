f696bb743c1e5c3e31924c7231dbdd50fd8adcd6
Marina Madrid García <74628083+MarinaMadrid@users.noreply.github.com>
2024-09-25 13:48:41 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("start-button").addEventListener("click", function() {
-    const overlay = document.getElementById("circle-overlay");
-    overlay.classList.add("hidden");
-
-    const gallery = document.querySelector(".photo-gallery");
-    gallery.classList.add("visible");
-});
